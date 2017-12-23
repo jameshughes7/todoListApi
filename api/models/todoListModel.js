f31bc6f1abf2131c44abfb19fee0c7055efe1263
James Hughes <hugjimbo77@gmail.com>
2017-12-23 21:31:01 +0000

    added express middleware

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -1,12 +1,12 @@
 'use strict';
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var TaskSchema = new Schema ({
+
+var TaskSchema = new Schema({
   name: {
     type: String,
-    required: "Kindly enter the name of the task"
+    Required: 'Kindly enter the name of the task'
   },
   Created_date: {
     type: Date,
@@ -14,9 +14,11 @@ var TaskSchema = new Schema ({
   },
   status: {
     type: [{
+      type: String,
       enum: ['pending', 'ongoing', 'completed']
     }],
     default: ['pending']
   }
 });
+
 module.exports = mongoose.model('Tasks', TaskSchema);
